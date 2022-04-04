db8df6b1351ae06c79dd1fbb861d463ba868c47e
mje <mjebsks.ac.kr>
2022-04-04 10:00:35 +0900

    php start

---
+++ form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">my project</a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">main</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/hello.php">hello word</a>
+    </li>
+
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Dropdown link
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">main</a>
+        <a class="dropdown-item" href="/login.php">Login</a>
+        <a class="dropdown-item" href="/form.php">serch</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container">
+  <form action="/action_page.php">
+      <h3>검색</h3>
+    <div class="form-group">
+      <input type="text" class="form-control" id="text" placeholder="검색어입력" name="text">
+    </div>
+    <button type="submit" class="btn btn-primary">검색</button>
+  </form>
+</div>
+
+</body>
+</html>
